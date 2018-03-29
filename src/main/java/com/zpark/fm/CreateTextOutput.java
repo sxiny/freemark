0f2dd0286d50f6812ff64a50710870186fc3cf57
sxiny <748701592@qq.com>
2018-03-29 14:20:28 +0800

    第一次提交
    Signed-off-by: sxiny <748701592@qq.com>

---
+++ src/main/java/com/zpark/fm/CreateTextOutput.java
@@ -0,0 +1,46 @@
+package com.zpark.fm;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CreateTextOutput {
+
+    public static void main(String[] args) throws IOException, TemplateException {
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "zhangsan");
+        User user = new User();
+        user.setId(1);
+        user.setName("lisi");
+        map.put("user", user);
+        List list = new ArrayList();
+        list.add("香蕉");
+        list.add("梨");
+        list.add("苹果");
+        map.put("list", list);
+
+        Map city = new HashMap();
+        city.put("bj", "北京");
+        city.put("sh", "上海");
+        city.put("us", "美国");
+
+        map.put("citys", city);
+
+        Configuration configuration = new Configuration(Configuration.VERSION_2_3_23);
+        configuration.setDefaultEncoding("utf-8");
+        configuration.setDirectoryForTemplateLoading(new File("E:\\Workspaces\\SpringBoot_Freemarker\\src\\main\\resources\\template"));
+        Template template = configuration.getTemplate("index.html");
+        template.process(map, new FileWriter("F:\\index.html"));
+    }
+
+
+}
---
+++ src/main/java/com/zpark/fm/User.java
@@ -0,0 +1,38 @@
+package com.zpark.fm;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private Integer id;
+    private String name;
+
+    public User(int i, String 小白兔) {
+    }
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
